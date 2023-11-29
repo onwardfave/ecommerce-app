d3edabb16ff98819c4307e4d33f72ed57553f091
Sam Wambua <wambua.sam@gmail.com>
2023-11-29 12:10:05 +0300

    Set port to 3002 if it is null

--- product-service/src/index.ts
+++ product-service/src/index.ts
@@ -3,9 +3,14 @@ import { sequelize } from './models'; // Adjust the import path as needed
 import productRoutes from './routes/product.routes'; // Adjust the import path as needed
 
 const app = express();
-const PORT: number = process.env.PRODUCT_SERVICE_PORT ? parseInt(process.env.PRODUCT_SERVICE_PORT) : undefined || 3002;
+let PORT: number = process.env.PRODUCT_SERVICE_PORT ? parseInt(process.env.PRODUCT_SERVICE_PORT) : null || 3002;
 
 console.log("Port number: ", PORT)
+
+if (PORT == null) {
+    PORT = 3002;
+}
+
 app.use(express.json());
 
 app.get('/api/v0', productRoutes);
